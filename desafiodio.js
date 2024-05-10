158e3092f945dc7a648282ad0ef13faa6a7e3b2a
Danyllo Danesi <danyllo.danesi@gmail.comgit config --global user.name Danyllo>
2024-05-10 19:52:51 -0300

    first commit

---
+++ desafiodio.js
@@ -0,0 +1,16 @@
+function exp(num){
+    let exp = num;
+
+    if (exp <=1000)
+    {
+        println("Você está no elo Ferro")
+    }
+    else  if (exp >=1001 && exp <=2000)
+    {
+        println("Você está no elo Ferro")
+    }
+}
+
+let num =1000
+
+exp(num)
